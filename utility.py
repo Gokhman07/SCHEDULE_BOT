5c93c4d0d705e9f0da4405ad4e0a2fda2a61faea
Ruslan Gokhman <61827720+Gokhman07@users.noreply.github.com>
2020-11-14 17:36:49 +0200

    Update utility.py

--- utility.py
+++ utility.py
@@ -38,6 +38,7 @@ def get_keyboard():
 def get_url(name):
     cursor.execute(f"SELECT URL, conf_id, password  FROM  TEACHERS WHERE Name ='{name}'")
     data= list(cursor.fetchall())
+    cursor.close()
    
     
     conf=[]
